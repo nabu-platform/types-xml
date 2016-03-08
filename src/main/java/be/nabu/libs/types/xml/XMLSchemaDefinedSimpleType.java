c3d80d1a45cedad09dfed8d70d3373b21b8cd08b
alex <alex@nabu.be>
2016-03-08 11:43:27 +0100

    Used DefinedSimpleType

--- src/main/java/be/nabu/libs/types/xml/XMLSchemaDefinedSimpleType.java
+++ src/main/java/be/nabu/libs/types/xml/XMLSchemaDefinedSimpleType.java
@@ -1,9 +1,9 @@
 package be.nabu.libs.types.xml;
 
-import be.nabu.libs.types.api.DefinedType;
+import be.nabu.libs.types.api.DefinedSimpleType;
 import be.nabu.libs.types.api.Type;
 
-public class XMLSchemaDefinedSimpleType<T> extends XMLSchemaSimpleType<T> implements DefinedType {
+public class XMLSchemaDefinedSimpleType<T> extends XMLSchemaSimpleType<T> implements DefinedSimpleType<T> {
 
 	XMLSchemaDefinedSimpleType(XMLSchema schema, String name, Type superType, String baseTypeName) {
 		super(schema, name, superType, baseTypeName);
