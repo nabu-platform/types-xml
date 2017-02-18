6f5f68910e43dd044c4d2036c97574af9a7a49f1
Alexander Verbruggen <alex@nabu.be>
2017-02-18 22:11:14 +0100

    Make sure xsd types are marshallable & unmarshallable

--- src/main/java/be/nabu/libs/types/xml/XMLSchemaSimpleType.java
+++ src/main/java/be/nabu/libs/types/xml/XMLSchemaSimpleType.java
@@ -1,11 +1,13 @@
 package be.nabu.libs.types.xml;
 
 import be.nabu.libs.property.api.Value;
+import be.nabu.libs.types.api.Marshallable;
 import be.nabu.libs.types.api.SimpleType;
 import be.nabu.libs.types.api.Type;
+import be.nabu.libs.types.api.Unmarshallable;
 import be.nabu.libs.validator.api.Validator;
 
-public class XMLSchemaSimpleType<T> extends XMLSchemaType<T> implements SimpleType<T> {
+public class XMLSchemaSimpleType<T> extends XMLSchemaType<T> implements SimpleType<T>, Marshallable<T>, Unmarshallable<T> {
 
 	private String baseTypeName;
 
@@ -45,5 +47,17 @@ public class XMLSchemaSimpleType<T> extends XMLSchemaType<T> implements SimpleTy
 		while (type.getSuperType() != null)
 			type = type.getSuperType();
 		return (Validator<T>) type.createValidator(values);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public T unmarshal(String content, Value<?>... values) {
+		return ((Unmarshallable<T>) getSuperType()).unmarshal(content, values);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public String marshal(T object, Value<?>... values) {
+		return ((Marshallable<T>) getSuperType()).marshal(object, values);
 	}
 }
